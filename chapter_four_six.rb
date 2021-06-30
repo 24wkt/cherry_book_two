3c20a375e97364befe936a529c191387f1a26cbd
24wkt <05wkt38xy.waku@gmail.com>
2021-06-30 16:06:21 +0900

    [4.6]rgb変換プログラムを作成する3

--- chapter_four_six.rb
+++ chapter_four_six.rb
@@ -32,10 +32,111 @@ end
 
 1) Error:
 RgbTest#test_to_hex:
-NoMethodError: undefined method `to_hex' for #<RgbTest:0x00007fe82e8e1570>
-    rgb_test.rb:5:in `test_to_hex'
+NoMethodError: undefined method 'to_hex' for #<RgbTest:0x00007fe82e8e1570>
+    rgb_test.rb:5:in test_to_hex
 
 1 runs, 0 assertions, 0 failures, 1 errors, 0 skips
 
 # ==> 案の定、テストは失敗しました。NoMethodErrorなので、「to_hex」が見つからない、定義されていないと言われています。
 
+#それからrgb.rbを作成して下記内容を記述する。(別途作成済み)
+
+def to_hex(r, g, b)
+    '#000000'
+  end
+
+#ここではあえてロジックらしいロジックを書かずに、あえて'#000000'という固定の文字列を返しています。
+#これはまず、テストコードがちゃんと機能しているか、とういうことをチェックするためです。
+
+#テストファイルに今回作成したrgb.rbをテストコードに読み込ませます。
+
+  require 'minitest/autorun'
+  require_relative './rgb.rb'　# <== 新しく追加
+
+  class RgbTest < Minitest::Test
+    def test_to_hex
+      assert_equal '#000000', to_hex(0, 0, 0)
+    end
+  end
+
+#ひとまず、テストを実行してみましょう！
+  Run options: --seed 4503
+  # Running:
+  .
+
+
+  Finished in 0.000717s, 1394.6999 runs/s, 1394.6999 assertions/s.
+
+  1 runs, 1 assertions, 0 failures, 0 errors, 0 skips
+
+#期待通り、テストをパスできました。しかし、これで満足してはいけません。テストコードにはもう一つ検証コードを追加します。
+
+require 'minitest/autorun'
+require_relative './rgb.rb'
+
+class RgbTest < Minitest::Test
+  def test_to_hex
+    assert_equal '#000000', to_hex(0, 0, 0)
+    assert_equal '#ffffff', to_hex(255, 255, 255)   # <== 新しく追加
+  end
+end
+
+#テストを実行してみても、当たり前ですが失敗します。
+
+1) Failure:
+RgbTest#test_to_hex [rgb_test.rb:7]:
+Expected: "#ffffff"
+  Actual: "#000000"
+
+1 runs, 2 assertions, 1 failures, 0 errors, 0 skips
+
+#ここで必要な作業は整数値を16進数に変換することです。これは「to_sメソッド」を使うと実現できます。
+
+  puts 0.to_s(16)     # ==> "0"
+  puts 255.to_s(16)   # ==> "ff"
+
+#基本的にはto_sメソッドで16進数に変換できます。
+#ところが、0の時は1桁の"0"になっています。こちらが期待しているのは"00"のような2桁の文字列です。
+#今回の場合だとrjustメソッドを使って右寄せすると便利です。第1引数には桁数を指定します。デフォルトは空白(半角スペース)で桁揃えされますが、第2引数を指定すると、空白以外の文字列を埋めることができます。
+
+  puts '0'.rjust(5)       # ==> "    0"
+  puts '0'.rjust(5, '0')  # ==> "00000"
+  puts '0'.rjust(5, '_')  # ==> "____0"
+
+#このメソッドを組み合わせれば、0を2桁の"00"に変換できますね。
+
+  puts 0.to_s(16).rjust(2, '0')     # ==> "00"
+  puts 255.to_s(16).rjust(2, '0')   # ==> "ff"
+
+#次のようにrgb.rb内のto_hexメソッドをちゃんと実装できます。
+  def to_hex(r, g, b)
+    '#' +
+      r.to_s(16).rjust(2, "0") +
+      g.to_s(16).rjust(2, "0") +
+      b.to_s(16).rjust(2, "0")
+  end
+
+#テストをパスできるか確認しましょう！
+  # Running:
+
+  .
+
+  Finished in 0.000602s, 1661.1298 runs/s, 3322.2595 assertions/s.
+
+  1 runs, 2 assertions, 0 failures, 0 errors, 0 skips
+
+#テストもパスすることができました。
+#念のため、r、g、bの各値がバラバラになっているテストも追加します。
+
+require 'minitest/autorun'
+require_relative './rgb.rb'
+
+class RgbTest < Minitest::Test
+  def test_to_hex
+    assert_equal '#000000', to_hex(0, 0, 0)
+    assert_equal '#ffffff', to_hex(255, 255, 255)
+    assert_equal '#043c78', to_hex(4, 60, 120)      # <== 新しく追加
+  end
+end
+
+#テストをパスしたら今度はリファクタリングをおこないます！
